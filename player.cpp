b7fc829ab0d6c3a05ace5c3e9920d52dec2bcfe3
chale11 <chale11@alaska.edu>
2020-02-23 18:08:13 -0900

    added more to player

--- player.cpp
+++ player.cpp
@@ -1,9 +1,15 @@
+#include <string>
+#include "player.h"
 
-class Player{
-private:
-    unsigned int score;
-public:
+Player::Player(std::string str): _playerName(str){
+    //make ships associated with this class
+    //make something to hold the position of those ships
+}
 
 
-
-};
+std::string Player::getName(std::string){
+    return this->_playerName;
+}
+unsigned int Player::getScore(){
+    return this->_score;
+}
--- player.h
+++ player.h
@@ -1,1 +1,15 @@
+#ifndef FILE_PLAYER_H_INCLUDED
+#define FILE_PLAYER_H_INCLUDED
 
+#include <string>
+
+class Player{
+private:
+    unsigned int _score;
+    std::string _playerName;
+public:
+    Player(std::string);
+    std::string getName(std::string);
+    unsigned int getScore();
+};
+#endif
